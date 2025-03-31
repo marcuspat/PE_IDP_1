7f6d3dc1cb2eb994fb1f4f5a696d297a61d32004
Marcus Patman <marcus.patman@gmail.com>
2025-03-31 12:39:48 -0600

    ...

--- terraform/main.tf
+++ terraform/main.tf
@@ -2,18 +2,28 @@ provider "google" {
   project = "peidp01"
   region  = "us-central1"
 }
-resource "google_container_cluster" "pe_idp_1" {
-  name     = "pe-idp-1-cluster"
+
+resource "google_container_cluster" "peidp01_pe_idp_1" {
+  name     = "peidp01-pe-idp-1-cluster"
   location = "us-central1-a"
   node_pool {
-    name       = "default-pool"
+    name       = "peidp01-default-pool"
     node_count = 2
-    machine_type = "e2-standard-2"
+
+    node_config {
+      machine_type = "e2-standard-2"
+    }
   }
   # Enable Workload Identity for Secrets Manager
   workload_identity_config {
-    workload_pool = "${var.project_id}.svc.id.goog"
+    workload_pool = "peidp01.svc.id.goog"
   }
 }
-output "cluster_endpoint" { value = google_container_cluster.pe_idp_1.endpoint }
-output "cluster_ca_certificate" { value = google_container_cluster.pe_idp_1.master_auth.0.cluster_ca_certificate }
+
+output "cluster_endpoint" {
+  value = google_container_cluster.peidp01_pe_idp_1.endpoint
+}
+
+output "cluster_ca_certificate" {
+  value = google_container_cluster.peidp01_pe_idp_1.master_auth.0.cluster_ca_certificate
+}
